814d7cce8c7af262d2876ca834d07770cb86837c
Thom Allen <thomwork@gmail.com>
2016-06-23 12:55:37 +0100

    Thursday updates

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,65 @@
   name (optional): name to show in dropdown select option
   */
   var ads = [
+    { src: '---', name: 'Manila' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '---', name: 'Frankfurt' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '---', name: 'Shanghai' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
+    { src: '300x250', name: 'beach generic 320x250' },
+    { src: '480x320', name: 'beach gen 480x320' },
+    { src: '728x90' },
     { src: '300x250', name: 'beach generic 320x250' },
     { src: '480x320', name: 'beach gen 480x320' },
     { src: '728x90' },
   ];
   // populate the select from the ads array
   for (var i = 0; i < ads.length; i++) {
-    // use directory name as name if not set in array
-    if (!ads[i].name) {
-      ads[i].name = ads[i].src;
+    // set src property to 3 dashes for an optgroup
+    // name property defines the label
+    if (ads[i].src === '---') {
+      select.append('<optgroup label="' + ads[i].name + '">');
+    } else {
+      // use directory name as name if not set in array
+      if (!ads[i].name) {
+        ads[i].name = ads[i].src;
+      }
+      select.append('<option value="' + ads[i].src + '">' + ads[i].name + '</option>' );
     }
-    select.append('<option value="' + ads[i].src + '">' + ads[i].name + '</option>' );
   }
   // handle a change of value on the select
   select.change(function() {
