fad3b3b3f9080b2b1b4c91d281a1279846224375
Thom Allen <thomwork@gmail.com>
2016-06-27 16:14:40 +0100

    Rebuild select w/ jquery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 (function($) {
   // dom
-  var select = $('#adselect');
   var iFrame = $('#ad');
   var selectValue;
+  var reSelect = $('.reselect');
+  var reSelectItems = $('.reselect__option');
   /* array of ads we want to show
   src: iframe src/directory name
   name (optional): name to show in dropdown select option
@@ -13,67 +14,72 @@
     { src: '480x320', name: 'beach gen 480x320' },
     { src: '728x90' },
     { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
     { src: '---', name: 'Frankfurt' },
-    { src: '300x250', name: 'beach generic 320x250' },
     { src: '480x320', name: 'beach gen 480x320' },
     { src: '728x90' },
     { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '---', name: 'Shanghai' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
-    { src: '300x250', name: 'beach generic 320x250' },
-    { src: '480x320', name: 'beach gen 480x320' },
-    { src: '728x90' },
   ];
+  
   // populate the select from the ads array
-  for (var i = 0; i < ads.length; i++) {
-    // set src property to 3 dashes for an optgroup
-    // name property defines the label
-    if (ads[i].src === '---') {
-      select.append('<optgroup label="' + ads[i].name + '">');
-    } else {
-      // use directory name as name if not set in array
-      if (!ads[i].name) {
-        ads[i].name = ads[i].src;
+  function newPopulate() {
+    for (var i = 0; i < ads.length; i++) {
+      // set src property to 3 dashes for an optgroup
+      // name property defines the label
+      if (ads[i].src === '---') {
+        reSelect.append('<li class="reselect__label">' + ads[i].name + '</li>');
+      } else {
+        // use directory name as name if not set in array
+        if (!ads[i].name) {
+          ads[i].name = ads[i].src;
+        }
+        // append the item
+        reSelect.append('<li class="reselect__option" data-src="' + ads[i].src + '">' + ads[i].name + '</li>' );
       }
-      select.append('<option value="' + ads[i].src + '">' + ads[i].name + '</option>' );
     }
+    console.log('newPopulate() called');
   }
-  // handle a change of value on the select
-  select.change(function() {
-    selectValue = $(this).val();
-    console.log('changed to', selectValue);
-    iFrame.attr('src', 'ads/' + selectValue);
+  
+  // update the iframe src value
+  function updateIframeSrc(src) {
+    iFrame.attr('src', 'ads/' + src);
+    console.log('iframe src changed to:', src);
+  }
+  
+  // initialize after population
+  function init() {
+    // set active class on first elem in select list
+    var firstItem = $('.reselect__option').first();
+    firstItem.addClass('active visible');
+    // set initial iframe attribute to first item
+    updateIframeSrc(firstItem.attr('data-src'))
+  }
+  
+  // handlers for reSelect
+  // focus in
+  reSelect.attr('tabindex', -1).on('focus', function(event) {
+    event.stopPropagation();
+    $(this).addClass('interact');
+    $(this).children().addClass('visible');
+    // handle a click on child while focused
+    $('.reselect__option').on('click', function() {
+      updateIframeSrc($(this).attr('data-src'));
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+    });
+    console.log('focus in');
   });
+  
+  // focus out
+  reSelect.attr('tabindex', -1).on('focusout', function(event) {
+    event.stopPropagation();
+    $(this).removeClass('interact');
+    $(this).children().removeClass('visible');
+    console.log('focus out');
+  });
+
+  newPopulate();
+  init();
+  
 })(jQuery);
 
 //# sourceMappingURL=main.js.map
